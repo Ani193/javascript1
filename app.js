2e630b83539c9ba4bf565215acaa20f8ba83a4cb
Ani193 <anighoniyyahkh@gmail.com>
2024-11-13 11:09:55 +0700

    karyawan 1

---
+++ app.js
@@ -0,0 +1,65 @@
+// console.log("Hello World")
+
+// let nama = "Ani"
+
+// console.log(nama)
+
+// let greeting = `Hello nama saya ${nama} `
+
+// console.log(greeting)
+
+
+let daftarSiswa = ["Ana", "Diva", "cindy", "Ima", "Tyas"]
+
+let jumlah=0
+for (let i =0; i< daftarSiswa.length; i++) {
+    if (daftarSiswa [i][0] == "A") {
+        console.log("Ketemu:", daftarSiswa[i])
+        jumlah++;
+    }
+}
+
+console.log("jumlah:", jumlah)
+
+
+// console.log(daftarSiswa[1])
+
+//  for (let i =0; i<daftarSiswa.length; i++) {
+    // console.log(`${i+1}. ${daftarSiswa[i]}`)
+//  }
+
+//  for(let nama of daftarSiswa) {
+    // console.log(nama)
+// }
+
+//for(let index in daftarSiswa) {
+    // console.log(index)
+// }
+
+// daftarSiswa[2] = "Apsha"
+// daftarSiswa[3] = "Aini"
+// console.log(daftarSiswa)
+
+// daftarSiswa[0] = "A"
+// daftarSiswa[1] = "D"
+// daftarSiswa[2] = "C"
+// daftarSiswa[3] = "I"
+// daftarSiswa[4] = "T"
+// daftarSiswa[5] = "W"
+// daftarSiswa[6] = "A"
+// daftarSiswa[7] = "A"
+// console.log(daftarSiswa)
+     
+    //  console.log(`1. ${daftarSiswa[0] = "A"}`)
+    //  console.log(`2. ${daftarSiswa[1] = "D"}`)
+    //  console.log(`3. ${daftarSiswa[2] = "C"}`)
+    //  console.log(`4. ${daftarSiswa[3] = "I"}`)
+    //  console.log(`5. ${daftarSiswa[4] = "T"}`)
+    //  console.log(`6. ${daftarSiswa[5] = "W"}`)
+    //  console.log(`7. ${daftarSiswa[6] = "A"}`)
+    //  console.log(`8. ${daftarSiswa[7] = "A"}`)
+
+   
+
+
+
---
+++ job.js
@@ -0,0 +1,38 @@
+// (job 1)
+// let umurAdi = "16"
+// let umurZakky = "17"
+
+// let selisih = umurAdi - umurZakky
+// console.log("Umur Adi adalah", umurAdi, "Umur Zakky adalah", umurZakky, "selisih mereka adalah", selisih)
+ 
+// (job 2)
+// let hargaBaju = "70000"
+// let diskon = "10"
+// let harga = hargaBaju * diskon/100
+// let total = hargaBaju - diskon 
+// console.log("harga yang harus dibayar adalah", total, "anda berhemat", harga)
+
+// (job 3)
+// let durasiParkir= "2"
+// let tarifParkirPerjam = "2000"
+
+// let harga = (durasiParkir-2)*(tarifParkirPerjam)+(tarifParkirPerjam) 
+// console.log("Anda parkir selama",durasiParkir,"jam", "dengan total biaya sejumlah Rp",+harga)
+
+// (job 4)
+// let botolMinum = "600"
+// for (let i =0; i< 5; i++) {
+    // console.log("Rudi meminum air dari botol sebanyak 50ml, sebanyak 5 kali ");
+    // botolMinum -= 50;
+    // console.log(`Botol Minum Rudi sekarang berisi ${botolMinum}ml air`)
+// }
+
+// (job 6)
+function lowest(a, b, c){
+    return Math.min (a, b, c)
+}
+
+console.log(lowest(0,2,3))
+console.log(lowest(2,0,3))
+console.log(lowest(2,3,0))
+console.log(lowest(3,2,0))
